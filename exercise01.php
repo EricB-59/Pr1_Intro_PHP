899ff5bcab0a49b39e57efbed82f5ad2f0dfe578
EricB-59 <ebaenac59@gmail.com>
2024-09-17 18:22:38 +0200

    Eric

---
+++ exercise01.php
@@ -0,0 +1,13 @@
+<?php
+// Variables numéricas
+$numero1 = 2;
+$numero2 = 3;
+
+// Operaciones matematicas
+$sumar = $numero1 + $numero2;
+$resta = $numero1 - $numero2;
+$division = $numero1 / $numero2;
+
+echo "La suma de $numero1 y $numero2 es igual $sumar";
+echo "La resta de $numero1 y $numero2 es igual $resta";
+echo "La division de $numero1 y $numero2 es igual $division";
