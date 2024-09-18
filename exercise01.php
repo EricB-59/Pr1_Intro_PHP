0f9c0f97f703bf887337fe61a32c7ff86bd81fab
EricB-59 <ebaenac59@gmail.com>
2024-09-18 20:17:34 +0200

    Eric

--- exercise01.php
+++ exercise01.php
@@ -8,6 +8,7 @@ $sumar = $numero1 + $numero2;
 $resta = $numero1 - $numero2;
 $division = $numero1 / $numero2;
 
+echo "------SUMA-----RESTA------DIVISIÓN-----<br>";
 echo "La suma de $numero1 y $numero2 es igual $sumar"."<br>";
 echo "La resta de $numero1 y $numero2 es igual $resta"."<br>";
 echo "La division de $numero1 y $numero2 es igual $division";
@@ -15,4 +16,23 @@ echo "La division de $numero1 y $numero2 es igual $division";
 // Muestra cuál es mayor, cuál menor o si son iguales.
 //3. Si las dos variables son valores superiores a 1,
 //a. calcula el área del triángulo con base y altura igual a los valores de las variables.
+echo "<br><br>------COMPARACIÓN-----<br>";
 
+if($numero1 > $numero2){
+    echo "$numero1 es mayor a $numero2"."<br>";
+} else {
+    echo "$numero2 es mayor a $numero1"."<br>";
+}
+
+echo "<br><br>------SUPERIOR A 1-----<br>";
+if($numero1 > 1){
+    echo "$numero1 es mayor a 1"."<br>";
+}else{
+    echo "$numero1 es menor a 1"."<br>";
+}
+if($numero2 > 1){
+    echo "$numero2 es mayor a 1"."<br>";
+}
+else{
+    echo "$numero2 es menor a 1"."<br>";
+}
--- exercise03.php
+++ exercise03.php
@@ -4,3 +4,24 @@
 // 3. Muestra la progresión numérica desde la segunda variable hasta 0 con un bucle while.
 // 4. Muestra la progresión numérica desde la primera variable a la segunda con un bucle do/while.
 //a. Si la segunda variable es más pequeña, sólo queremos que imprima una vez el valor de la primera variable
+$numero1 = "32";
+echo $numero1."<br>";
+$numero2 = "15";
+
+echo "// 2. Muestra la progresión numérica de los números pares desde 0 hasta el valor de la primera con un bucle for.<br>";
+for ($i = 0; $i < $numero1; $i++) {
+    if  ($i%2 == 0) {
+        echo "$i | ";
+    }
+}
+
+echo "<br><br>// 3. Muestra la progresión numérica desde la segunda variable hasta 0 con un bucle while.<br>";
+while (!$numero2 == 0){
+    echo "$numero2 | ";
+    $numero2 = $numero2 - 1;
+}
+
+echo "<br><br>// 4. Muestra la progresión numérica desde la primera variable a la segunda con un bucle do/while.<br>";
+do {
+    
+} while ($a <= 10);
