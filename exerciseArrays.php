c4f463a0881882e0d8f1769bc25a0d6d400b8e03
EricB-59 <ebaenac59@gmail.com>
2024-09-25 20:21:53 +0200

    eric

--- exerciseArrays.php
+++ exerciseArrays.php
@@ -1,1 +1,29 @@
 <?php
+$person = array("nombre"=>"Sara", "apellido"=>"Martínez", "edad"=>23, "ciudad"=>"Barcelona");
+$i = 0;
+echo "EJERCICIO 1<br>";
+foreach ($person as $x => $y) {
+    $i++;
+    echo "dato $i º: $y <br>";
+}
+
+$i = 0;
+echo "<br>EJERCICIO 2<br>";
+foreach ($person as $x => $y) {
+    $i++;
+    echo "$x: $y <br>";
+}
+
+echo "<br>EJERCICIO 3<br>";
+$person = array("nombre"=>"Sara", "apellido"=>"Martínez", "edad"=>24, "ciudad"=>"Barcelona");
+$i = 0;
+foreach ($person as $x => $y) {
+    $i++;
+    echo "$x: $y <br>";
+}
+
+echo "<br>EJERCICIO 4<br>";
+$person = array("nombre"=>"Sara", "apellido"=>"Martínez", "edad"=>24);
+var_dump($person);
+
+echo "<br>EJERCICIO 5<br>";
