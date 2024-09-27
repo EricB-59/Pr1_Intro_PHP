7bb46a56ed99bc8d4ef160f23f7e9d7345dc1cbe
EricB-59 <151823587+EricB-59@users.noreply.github.com>
2024-09-27 19:16:50 +0200

    eric

--- exerciseArrays.php
+++ exerciseArrays.php
@@ -15,15 +15,30 @@ foreach ($person as $x => $y) {
 }
 
 echo "<br>EJERCICIO 3<br>";
-$person = array("nombre"=>"Sara", "apellido"=>"Martínez", "edad"=>24, "ciudad"=>"Barcelona");
+$person["edad"] = 24;
 $i = 0;
 foreach ($person as $x => $y) {
     $i++;
-    echo "$x: $y <br>";
+    echo "dato $i º: $y <br>";
 }
 
 echo "<br>EJERCICIO 4<br>";
-$person = array("nombre"=>"Sara", "apellido"=>"Martínez", "edad"=>24);
+unset($person["ciudad"]);
 var_dump($person);
 
-echo "<br>EJERCICIO 5<br>";
+echo "<br><br>EJERCICIO 5<br>";
+$letters = "a,b,c,d,e,f";
+$arrayLetters = explode(",", $letters);
+
+for($i = count($arrayLetters); $i > 0; $i--) {
+    echo "letter $i".$arrayLetters[$i-1]."<br>";
+}
+
+echo "<br><br>EJERCICIO 6<br>";
+$notas = array("Miguel" => 5, "Luís" => 7, "Marta" => 10, "Isabel" => 8, "Aitor" => 4, "Pepe" => 1);
+arsort($notas);
+
+echo "Notas de los estudiantes: ";
+foreach ($notas as $nombre => $nota) {
+    echo $nombre.":".$nota."\n";
+}
