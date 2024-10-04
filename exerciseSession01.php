ef1ca7ba72d17defa405c5bef2a7e732e62865ce
EricB-59 <151823587+EricB-59@users.noreply.github.com>
2024-10-04 17:50:52 +0200

    eric

--- exerciseSession01.php
+++ exerciseSession01.php
@@ -1,14 +1,16 @@
 <?php
 session_start();
 $_SESSION["work-name"] = $_GET["worker-name"];
-$product = $_SESSION["product"] = $_GET["product"];
-$quantity = $_SESSION["quantity"] = $_GET["quantity"];
+$product = $_GET["product"];
+$quantity = $_GET["quantity"];
+
+$quantity_milk = $_SESSION["product-quantity-milk"];
+$quantity_softdrink = $_SESSION["product-quantity-softdrink"];
 
 if (!isset($product) && !isset($quantity)) {
-    $quantity = $_GET["quantity"];
     switch ($product) {
         case 'milk':
-            $_SESSION["product-quantity-milk"] += $quantity;
+            $_SESSION["product-quantity-milk"] += 1;
             break;
         case 'soft-drink':
             $_SESSION["product-quantity-softdrink"] += $quantity;
@@ -40,20 +42,17 @@ if (!isset($product) && !isset($quantity)) {
         </select><br>
 
         <label for="quantity">Product quantity:</label>
-        <input type="number" id="quantity" name="quantity" value="">
+        <input type="number" id="quantity" name="quantity" value=""><br>
 
         <input type="submit" value="ADD">
         <button type="button">REMOVE</button>
         <input type="reset" value="RESET">
-
-        <div class="inventory">
-            <h3>Inventory:</h3>
-            <p id="inventory-worker">Worker: <?php echo $_SESSION["work-name"]?></p>
-            <p id="inventory-milk">Units milk: <?php echo $_SESSION["product-quantity-milk"]?></p>
-            <p id="inventory-soft-drink">Units soft drink: <?php echo $_SESSION["product-quantity-softdrink"]?></p>
-        </div>
     </form>
-    
-
+    <div class="inventory">
+        <h3>Inventory:</h3>
+        <p id="inventory-worker">Worker: <?php echo $_SESSION["work-name"]?></p>
+        <p id="inventory-milk">Units milk: <?php echo $_SESSION["product-quantity-milk"]?></p>
+        <p id="inventory-soft-drink">Units soft drink: <?php echo $_SESSION["product-quantity-softdrink"]?></p>
+    </div>
 </body>
 </html>
